59062eadf2df765ee227c2569ce45e39cb0ce177
weare4saken <kondrashka91@gmail.com>
2023-04-13 15:03:06 +0700

    initial commit

---
+++ src/main/java/com/skypro/pastebinanalog/PastebinAnalogApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.pastebinanalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinAnalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinAnalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/pastebinanalog/PastebinAnalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.pastebinanalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastebinAnalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
